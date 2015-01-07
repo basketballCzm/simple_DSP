3cae420ebe912eb8782f9a291d9f2ade1a2f73fe
Jason <1185340883@qq.com>
2015-01-07 15:00:01 +0800

    test

--- adstat.cpp
+++ adstat.cpp
@@ -93,6 +93,9 @@ int main()
             syslog(LOG_INFO, "adstat key=%d",strlen(p_para->key));
             syslog(LOG_INFO, "adstat value=%d",p_para->value);
             FCGX_FPrintF(request.out, "<br />key : %s ,value : %s",p_para->key,p_para->value);
+			if (strcmp(p_para->key,"adid"))
+				 FCGX_FPrintF(request.out," Â Â…Ù…ÙøÕªß");
+
         }
 
 
