36eb7fc4ffb55775cfa94858eff434b9f1b42f2c
Caster Zeng <zxc262403@gmail.com>
2015-01-08 11:00:25 +0800

    1.add source ad_request to handle ad show request and do ad bidding.

--- restart_fastcgi.sh
+++ restart_fastcgi.sh
@@ -3,3 +3,6 @@ pkill adstat
 
 pkill ad_register
 /usr/local/nginx/sbin/spawn-fcgi -a 127.0.0.1 -p 9001 -C 25 -f  /usr/local/nginx/simple_DSP_cgi/bin/ad_register -F 10
+
+pkill ad_request
+/usr/local/nginx/sbin/spawn-fcgi -a 127.0.0.1 -p 9002 -C 25 -f  /usr/local/nginx/simple_DSP_cgi/bin/ad_request -F 10
