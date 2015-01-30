c379e9531b04f937a55daa12602d6d7896be8b70
Caster Zeng <zxc262403@gmail.com>
2015-01-30 18:23:18 +0800

    1.add ad bidding module to select one of the highest eCPM ADs to show.

--- ad_request.cpp
+++ ad_request.cpp
@@ -17,7 +17,12 @@
 #include "config.h"
 
 #include "user_map.h"
+#include "math.h"
+#define NONE_INT_DATA INT_MIN
 
+#define NEW_AD_MIN_SHOW_COUNT 200
+#define NEW_AD_CTR 0.05
+#define HIGHEST_N_ADS 3 
 
 using namespace std;
 
@@ -56,10 +61,93 @@ static void penv(const char * const * envp)
 
 }
 
-static void bidding(UriQueryListA * queryList,Json::Value & ret)
+static float get_eCPM(AdInfo & ad)
 {
-    int user_id=INT_MIN;
-    int ad_space=INT_MIN;//TODO now ,only have one ad space.
+    if(int(ad.show_counter)<NEW_AD_MIN_SHOW_COUNT)
+    {
+        return 1000.0*ad.show_price+1000.0*ad.click_price*NEW_AD_CTR; 
+    }
+    else
+    {
+        return 1000.0*ad.show_price+1000.0*ad.click_price*int(ad.click_counter)/int(ad.show_counter); 
+    }
+}
+
+
+static void bidding(Json::Value &ret,const UserPosition &pos )
+{
+    int highest_ad_index_list[HIGHEST_N_ADS]{};
+    float highest_eCPM_list[HIGHEST_N_ADS]{};
+  
+    for(int i=0;i< int(g_p_ad_data->number);++i)
+    {
+        if(g_p_ad_data->ad_list[i].valid==0)
+            continue;
+
+        if(pos.position.z!=NONE_INT_DATA) 
+        {
+            float distance= fabsf(g_p_ad_data->ad_list[i].ad_point.x-pos.position.x)+
+                fabsf(g_p_ad_data->ad_list[i].ad_point.y-pos.position.y);
+            if(distance> g_p_ad_data->ad_list[i].ad_radius)
+                continue;
+        }
+        float eCPM=get_eCPM(g_p_ad_data->ad_list[i]);
+        
+        for(int j=HIGHEST_N_ADS-1;j<=0;--j)
+        {
+            if(eCPM > highest_eCPM_list[j] )    
+            {
+                if(j==HIGHEST_N_ADS-1)
+                {
+                    highest_eCPM_list[j]=eCPM;
+                    highest_ad_index_list[j]=i;
+                }
+                else
+                {
+                    highest_eCPM_list[j+1]=highest_eCPM_list[j];
+                    highest_ad_index_list[j+1]=highest_ad_index_list[j];
+                    highest_eCPM_list[j]=eCPM;
+                    highest_ad_index_list[j]=i;
+                }
+            }
+            else
+            {
+                break;    
+            }
+        }
+    }
+    
+    int length=0;
+    for(int i=0;i<HIGHEST_N_ADS;++i)
+    {
+        if(highest_eCPM_list[i]!=0)
+            length++;
+        else
+            break;
+    }
+    if(length)
+    {    
+        int show_ad_no=rand()%length;
+        int show_ad_index=highest_ad_index_list[show_ad_no];
+        AdInfo & show_ad=g_p_ad_data->ad_list[show_ad_index];
+
+        atomic_fetch_add_explicit(& show_ad.show_counter,1, std::memory_order_seq_cst) ;
+        ret["content"]=show_ad.ad_content;
+        ret["type"]=show_ad.ad_type;
+        ret["id"]=show_ad.ad_id;
+        ret["result"]="ok";
+    }
+    else
+    {
+        ret["result"]="no valid ad";    
+    }
+}
+
+
+static void ad_request(UriQueryListA * queryList,Json::Value & ret)
+{
+    int user_id=NONE_INT_DATA;
+    int ad_space=NONE_INT_DATA;//TODO now ,only have one ad space.
     for(UriQueryListA *p_para=queryList ;p_para!=NULL;p_para=p_para->next)
     {
         if (strcmp(p_para->key,"user_id")==0)
@@ -71,8 +159,21 @@ static void bidding(UriQueryListA * queryList,Json::Value & ret)
             stringstream( p_para->value )>> ad_space;
         }
     }
-
     UserPosition pos;
+    pos.position.z=NONE_INT_DATA;//if's flag of no laction data
+    if(user_id==NONE_INT_DATA)
+    {
+        syslog(LOG_INFO, "adstat ad_request, no user id");   
+        return bidding(ret,pos);
+    }
+    pos.user_id=user_id;
+    if( user_query( pos) ==-1)
+        //user id not found!
+    {
+        syslog(LOG_INFO, "adstat ad_request, user id :%d , location data not found!",user_id);   
+    }
+    
+    return bidding(ret,pos);
 
 }
 
@@ -106,7 +207,7 @@ int main()
         FCGX_FPrintF(request.out, "Content-type:text/html\r\n\r\n" );
 
         Json::Value ret;
-        bidding(queryList,ret);
+        ad_request(queryList,ret);
         const string output = writer.write(ret);
         syslog(LOG_INFO, "[output : %s\n  ]", output.c_str()); 
         FCGX_FPrintF(request.out, output.c_str() );
--- user_map.h
+++ user_map.h
@@ -7,6 +7,6 @@ int user_add(int user_id, float x, float y, int z);
 int user_remove(int user_id);
 int user_update(UserPosition pos);
 int user_update(int user_id,float x,float y,int z);
-int user_query(UserPosition & user_id);
+int user_query(UserPosition & user_pos);
 
 void user_list_all(Json::Value & user_list);
