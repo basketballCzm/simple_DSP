dfc0899ff4b8c740cced2bd96cdeb7ed8b3eed0c
caster zeng <caster_zeng@wushuu.com>
2015-06-10 20:04:23 +0800

    1.add test data of location,ad.space,ad.group in ad_map.test.js 2.add auto clean test data from database function in ad_map.test.js 3.test function get_ad_group_set_of_space() in ad_map_test.cpp

--- test/ad_map.test.js
+++ test/ad_map.test.js
@@ -11,11 +11,28 @@ var exec = require('child_process').exec
 
 var tair;
 var nm=3;
-describe('client.test.js', function () {
+var used_key={}
+var mall_id=2
+
+function add_used_key(keys)
+{
+    if(keys.constructor === Array)
+    {
+      keys.forEach(function(e){
+          used_key[e]=1
+        })  
+    }
+    else
+    {
+      used_key[keys]=1
+    }
+}
+
+describe('ad_map.test.js', function () {
 
   before(function (done) {
     tair = new cli('group_1', [
-      {host: '192.168.2.201', port: 5198}
+      {host: 'localhost', port: 5198}
     ], {heartBeatInterval: 3000},
       function (err) {
         if (err) {
@@ -26,35 +43,167 @@ describe('client.test.js', function () {
       });
   });
   
-  it("insert ad.group test data should be ok!",function(done){
-      done()
+  it("ad_map insert test data location should be ok!",function(done){
+      var test_entry_list=[
+        ["123456-lt",4.3,3.8,1],
+        ["234567-rb",204,100.8,1],
+        ["345678-lb",4.3,103.8,1],
+        ["abcdef-rt",198.3,2.6,1],
+        ["bcdefg-center",100.15,50.3,1],
+      ]
+      var prop_list=["x","y","z"]
+      var count=0
+      test_entry_list.forEach(function(c){
+          prop_list.forEach(function(d,j){
+              var key="location:"+mall_id+":"+c[0]+":"+d
+              var value=c[j+1]
+              add_used_key(key)
+              tair.set(key,value,0,nm,0,function(err,success){
+                  should.not.exist(err)
+                  success.should.equal(true)
+                  count++;
+                  if(count==prop_list.length*test_entry_list.length){
+                    done()
+                  }
+                })
+            })
+        })
+    })
+
+  it("ad_map insert test data ad.space should be ok!",function(done){
+       var test_entry_list=[
+        [23,"space No.23",1,[3,2,4]],
+        [28,"space No.28",1,[1,5,6]],
+      ]
+      var prop_list=["desc","type","ad.group.set"]
+      var count=0
+      test_entry_list.forEach(function(c){
+          prop_list.forEach(function(d,j){
+              var key="ad.space:"+mall_id+":"+c[0]+":"+d
+              var value=c[j+1]
+              add_used_key(key)
+              if(d.indexOf("set",d.length-3)===-1)
+              {
+                tair.set(key,value,0,nm,0,function(err,success){
+                    should.not.exist(err)
+                    success.should.equal(true)
+                    count++;
+                    if(count==prop_list.length*test_entry_list.length){
+                      done()
+                    }
+                  })
+              }
+              else
+              {
+                var zcount=0
+                c[j+1].forEach(function(v){
+                    tair.zadd(key,nm,v,v,function(err, data){
+                        should.not.exist(err);
+                        data.should.equal(true);
+                        ++zcount
+                        if(zcount==c[j+1].length)
+                        {
+                          count++ 
+                          if(count==prop_list.length*test_entry_list.length){
+                            done()
+                          }
+                        }
+                      })
+                  })  
+              }
+            })
+        })
+      
+    })
+
+  it("ad_map insert test data ad.group should be ok!",function(done){
+       var test_entry_list=[
+        [1,1.0,0.01,23,1,[1,2,7],['label1','label2', 'label3']],
+        [2,1.1,0.02,23,1,[3],['label1','label3', 'label6']],
+        [3,1.2,0.016,23,1,[4],['label3','label4']],
+        [4,1.4,0.014,28,1,[5],['label4','label5']],
+        [5,1.5,0.013,28,1,[],['label3','label6']],
+        [6,1.6,0.011,28,1,[6],['label4','label7']],
+      ]
+      var prop_list=["click.price","show.price",
+        "space","valid","ad.set","target.label.set"
+      ]
+      var count=0
+      test_entry_list.forEach(function(c){
+          prop_list.forEach(function(d,j){
+              var key="ad.group:"+mall_id+":"+c[0]+":"+d
+              var value=c[j+1]
+              if(d.indexOf("set",d.length-3)===-1)
+              {
+                if(d.indexOf("price",d.length-5)!==-1)
+                {
+                  var tmpBuffer = new Buffer(8)
+                  tmpBuffer.writeDoubleLE(value,0)
+                  value= tmpBuffer
+                }
+                add_used_key(key)
+                tair.set(key,value,0,nm,0,function(err,success){
+                    should.not.exist(err)
+                    success.should.equal(true)
+                    count++;
+                    if(count==prop_list.length*test_entry_list.length){
+                      done()
+                    }
+                  })
+              }
+              else
+              {
+                var zcount=0
+                if(c[j+1].length>=1)
+                {
+                  add_used_key(key)
+                }
+                else
+                {
+                  count++
+                  if(count==prop_list.length*test_entry_list.length){
+                    done()
+                  }
+                }
+                c[j+1].forEach(function(v){
+                    tair.sadd(key,nm,v,function(err, data){
+                        should.not.exist(err);
+                        data.should.equal(true);
+                        ++zcount
+                        if(zcount==c[j+1].length)
+                        {
+                          count++ 
+                          if(count==prop_list.length*test_entry_list.length){
+                            done()
+                          }
+                        }
+                      })
+                  })  
+              }
+            })
+        })
     })
 
   it("c++ test ,ad_request should be ok!",function(done){
       exec(__dirname+'/ad_map_test',function(err,stdout,stderr){
           sys.print('stdout:'+ stdout)
           sys.print('stderr:'+ stderr)
-          if(err !== null)
-          {
-            console.log('exec error: '+err)
-          }
+          should.not.exist(err)
           done()
         })
     })
 
   it("clear up test data should work",function(done){
-      var key_list=['xiang','zadd.test.key','zadd.test.key2','sadd.int.test.key','alargeData',];
       var count=0
-      for(var i=0;i<key_list.length;i++){
-        console.log('remove '+key_list[i])
-        tair.remove(key_list[i],nm,function(err){
+      for(var key in used_key){
+        console.log('remove '+key)
+        tair.remove(key,nm,function(err){
           should.not.exist(err)
           count++;
-          if(count==key_list.length){
+          if(count==Object.keys(used_key).length){
              done();
           }
         })
       }
     })
-
 });
--- test/ad_map_test.cpp
+++ test/ad_map_test.cpp
@@ -1,4 +1,5 @@
 #include "ad_map.h"
+#include <ctime>
 #include "tair_common.h"
 #include "gtest/gtest.h"
 #include <data_entry.hpp>
@@ -10,4 +11,45 @@ TEST(TairCommon,GetDataEntry)
     float space_id=4.5;
     get_data_entry( key,"ad.space:",mall_id,":",space_id,":ad.group.set");
     EXPECT_STREQ("ad.space:2:4.5:ad.group.set",key.get_data());
+}
+
+
+using namespace ad_map;
+namespace ad_map{
+  void bidding(Json::Value &ret, const UserPosition &pos, const int space_id, const int mall_id);
+  void get_ad_group_set_of_space(const int mall_id, const int space_id, std::vector< int> &ad_group_set);
+  void get_ad_group_set_of_location(const int mall_id, const UserPosition &pos, std::vector<int> &ad_group_set);
+}
+class AdMapTest : public testing::Test
+{
+protected:  
+    virtual void SetUp()
+    {
+      ad_map_init();
+    }
+    virtual void TearDown()
+    {
+        //user_map::close();
+    }
+    const int mall_id=2;
+};
+
+TEST_F(AdMapTest,getAdGroupSetOfSpace)
+{
+  int space_id=23;
+  std::vector< int> ad_group_set;
+  get_ad_group_set_of_space(mall_id,space_id,ad_group_set);
+  EXPECT_EQ(ad_group_set.size(),3);
+  EXPECT_EQ(ad_group_set[0],2);
+  EXPECT_EQ(ad_group_set[1],3);
+  EXPECT_EQ(ad_group_set[2],4);
+}
+
+TEST_F(AdMapTest,Bidding)
+{
+  EXPECT_EQ(2,4);
+}
+
+TEST_F(AdMapTest,AdRequest)
+{
 }
