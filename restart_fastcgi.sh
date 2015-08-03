eb6df10d5d116f992f89f1387a09c518e20ef963
caster zeng <caster_zeng@wushuu.com>
2015-08-03 17:51:05 +0800

    1.update for chongqi

--- restart_fastcgi.sh
+++ restart_fastcgi.sh
@@ -1,5 +1,5 @@
 pkill user_op
-env spawn-fcgi -a 127.0.0.1 -p 9003 -C 25 -f  /usr/local/nginx/simple_DSP_cgi/bin/user_op -F 10
+env spawn-fcgi -a 127.0.0.1 -p 9003 -C 25 -f  /etc/nginx/simple_DSP_cgi/bin/user_op -F 10
 
 pkill ad_op
-env spawn-fcgi -a 127.0.0.1 -p 9004 -C 25 -f  /usr/local/nginx/simple_DSP_cgi/bin/ad_op -F 10
+env spawn-fcgi -a 127.0.0.1 -p 9004 -C 25 -f  /etc/nginx/simple_DSP_cgi/bin/ad_op -F 10
