6c2703bcf5b5ca57991ffd5fdb0b0e2b2f264811
caster zeng <caster_zeng@wushuu.com>
2015-11-03 16:50:58 +0800

    1.fix the bugs in restart shell script

--- restart_receiver.sh
+++ restart_receiver.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env sh
 pkill ap_signal_receiver
-/etc/nginx/simple_DSP_cgi/bin/ap_signal_receiver
-
 pkill user_tag_receiver 
-/etc/nginx/simple_DSP_cgi/bin/user_tag_receiver
+sleep 2
+
+/etc/nginx/simple_DSP_cgi/bin/ap_signal_receiver &
+/etc/nginx/simple_DSP_cgi/bin/user_tag_receiver &
