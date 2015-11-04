e4c4cc691c11e490a8dc0f0d8d5c588097af7082
caster zeng <caster_zeng@wushuu.com>
2015-11-04 15:32:35 +0800

    1.quick fix

--- restart_receiver.sh
+++ restart_receiver.sh
@@ -3,5 +3,5 @@ ps -e | grep ap_signal_receiver | awk '{print $1}'|xargs kill -9
 ps -e | grep user_tag_receiver | awk '{print $1}'|xargs kill -9 
 sleep 2
 
-nohup /etc/nginx/simple_DSP_cgi/bin/ap_signal_receiver &
-nohup /etc/nginx/simple_DSP_cgi/bin/user_tag_receiver &
+nohup ./ap_signal_receiver >/dev/null &
+nohup ./user_tag_receiver >/dev/null &
