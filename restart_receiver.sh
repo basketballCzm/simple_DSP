4e77a04523eb8930e6b84214ba33bf65143bc62b
caster zeng <caster_zeng@wushuu.com>
2015-11-05 11:59:10 +0800

    1.quick fix

--- restart_receiver.sh
+++ restart_receiver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-ps -e | grep ap_signal_receiver | awk '{print $1}'|xargs kill -9 
-ps -e | grep user_tag_receiver | awk '{print $1}'|xargs kill -9 
+ps -e | grep ap_signal_recei | awk '{print $1}'|xargs kill -9 
+ps -e | grep user_tag_receiv | awk '{print $1}'|xargs kill -9 
 sleep 2
 
 nohup ./ap_signal_receiver 1>/dev/null 2>&1 &
