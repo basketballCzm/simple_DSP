d7a681d400f45a4021176c1554ad10b3cd7b2bec
caster zeng <caster_zeng@wushuu.com>
2015-11-09 12:10:42 +0800

    1.quick fix

--- restart_fastcgi.sh
+++ restart_fastcgi.sh
@@ -3,5 +3,5 @@ pkill user_op
 pkill ad_op
 sleep 2
 
-env spawn-fcgi -a 127.0.0.1 -p 9003 -C 25 -f  ./user_op -F 10
-env spawn-fcgi -a 127.0.0.1 -p 9004 -C 25 -f  ./ad_op -F 10
+env spawn-fcgi -a 0.0.0.0 -p 9003 -C 25 -f  ./user_op -F 10
+env spawn-fcgi -a 0.0.0.0 -p 9004 -C 25 -f  ./ad_op -F 10
