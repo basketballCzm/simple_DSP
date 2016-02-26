c5e564fce65cad8c0be32571e40e46ab0948c861
gui <1527352582@qq.com>
2016-02-26 19:47:55 +0800

    fix MacSetDaily test bug

--- test/user_map_test.cpp
+++ test/user_map_test.cpp
@@ -28,19 +28,23 @@ protected:
         user_map_init();
         user_map::check_vip=1;
     }
+
     virtual void TearDown()
     {
-        //user_map::close();
+     
     }
-    float x=3.1415926535;
-    float y=2.718281828;
-    int z=4;
-    int mall_id=5;
-    int nm=2;
-    static vector<tair::common::data_entry> saved_keys;
+
 public:    
     static unsigned long long  mac;
     static int user_id;
+
+protected:
+    float x = 3.1415926535;
+    float y = 2.718281828;
+    int z = 4;
+    int mall_id = 5;
+    int nm = 2;
+    static vector<tair::common::data_entry> saved_keys;
 };
 
 int UserMapTest::user_id=0;
@@ -166,11 +170,11 @@ TEST_F(UserMapTest,VipArriveTime)
 TEST_F(UserMapTest,MacSetDaily)
 {
     tair::common::data_entry key;
-    const string s_date =  get_date_str(time(0));
-    get_data_entry(key,"mac.set:",s_date,":",mall_id,":daily");
+    get_data_entry(key, "mac.set:", get_date_str(time(0)), ":", mall_id, ":daily");
+
     saved_keys.push_back(key);
     vector<string> values;
-    tair_smembers(user_map::g_tair,nm,key,values);
+    tair_smembers(user_map::g_tair, nm, key, values);
     ASSERT_EQ(values.size(),2);
     EXPECT_STREQ(values[0].c_str(),"1234567890");
     EXPECT_STREQ(values[1].c_str(),"1234567891");
@@ -205,6 +209,9 @@ TEST_F(UserMapTest,RemoveKeys)
     saved_keys.push_back(key);
     get_data_entry(key,"location:",mall_id,":",mac,":time");
     saved_keys.push_back(key);
+    get_data_entry(key,"mac.set:",get_date_str(time(0)),":",mall_id,":daily");
+    saved_keys.push_back(key);
+
     for(vector<tair::common::data_entry>::iterator it= saved_keys.begin(); it != saved_keys.end(); ++it)
     {
         cout<<"remove key:"<<it->get_data()<<endl;
