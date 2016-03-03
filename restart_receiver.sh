32ff3da66f8f397937f3d427cf97b588b840b5c3
曾少臣 <zxc262403@gmail.com>
2016-03-03 17:01:36 +0800

    1.add log files of receivers

--- restart_receiver.sh
+++ restart_receiver.sh
@@ -4,6 +4,9 @@ ps -e | grep user_tag_receiv | awk '{print $1}'|sudo xargs kill -9
 ps -e | grep user_phone_rece | awk '{print $1}'|sudo xargs kill -9 
 sleep 2
 
-nohup ./bin/ap_signal_receiver 1>/dev/null 2>&1 &
-nohup ./bin/user_tag_receiver 1>/dev/null 2>&1 &
-nohup ./bin/user_phone_receiver 1>/dev/null 2>&1 &
+sudo mkdir -p /var/log/wushuu_dsp
+sudo chown caster:caster /var/log/wushuu_dsp
+
+nohup ./bin/ap_signal_receiver 1>/var/log/wushuu_dsp/ap_signal_receiver.log 2>&1 &
+nohup ./bin/user_tag_receiver 1>/var/log/wushuu_dsp/user_tag_receiver.log 2>&1 &
+nohup ./bin/user_phone_receiver 1>/var/log/wushuu_dsp/user_phone_receiver.log 2>&1 &
